6dcbed3830d55ae584f8ff4ff4bb745df9dae701
Xavier Fernandes <xfdes28@hotmail.com>
2026-05-31 00:39:29 +1200

    test

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
