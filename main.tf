18fb57619de2b20d8be13548010f49bf9b85dbcd
Xavier Fernandes <xfdes28@hotmail.com>
2026-05-31 00:32:04 +1200

    test

--- main.tf
+++ main.tf
@@ -4,3 +4,17 @@ resource "aws_security_group" "allow_tls1"{
   description = "Allow TLS inbound traffic and all outbound traffic"
 
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls1.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls1.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
