5dae127a5cabe5f9e9edb866cd23658352ab23a0
Xavier Fernandes <xfdes28@hotmail.com>
2026-05-31 00:33:02 +1200

    test

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "allow_tls1"{
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls1.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
