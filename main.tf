9f800a3eb12bea14422a1ddf5958f03d09af1dfd
Xavier Fernandes <xfdes28@hotmail.com>
2026-05-30 20:51:07 +1200

    test

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = var.my_bucket_name
 }
 
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "my_bucket1" {
   bucket = "my-xaviersavio-1899"
 }
